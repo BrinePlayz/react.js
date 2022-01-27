c55c4d3f8a33c751e6326e92ffa0a2d1dad23e2a
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2022-01-27 16:40:03 +0530

    `

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-global.React = require('react')
 require('express')()
 .set('view engine', 'jsx')
 .set('views', __dirname + '/pages')
 .engine('jsx', require('express-react-views').createEngine())
-.get('/', (req, res) => {
-    res.render('index')
-})
-.listen(8001, null, () => console.log('[Connected]'))
+.get('/', (req, res) => res.render('index'))
+.listen(1, null, () => console.log('[Connected]'))
