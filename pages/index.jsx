cca0e53f919d1f145a12f0a1fff3259cddc2c074
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2022-01-27 16:11:37 +0530

    1

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,16 +3,11 @@ const { useEffect } = require('react')
 module.exports = function() {
     let [count, setCount] = useEffect(0)
     return (
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>React.js Starter</title>
-        </head>
-        <body>
-            <button onClick={() => setCount(count + 1)}>This Button Is Clicked {count} Times</button>
-        </body>
-        </html>
-    )
+        <div>
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(count + 1)}>
+            Click me
+          </button>
+        </div>
+      );
 };
