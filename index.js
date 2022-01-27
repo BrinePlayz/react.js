f88a2ba966ed57d9fd08eed37165b4d30980b3a4
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2022-01-27 15:50:47 +0530

    1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = express()
 .set('view engine', 'jsx')
+.set('views', __dirname + '/pages')
 .engine('jsx', require('express-react-views').createEngine())
 .get('/', (req, res) => {
     res.render('index', { name: 'React' })
