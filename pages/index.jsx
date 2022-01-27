b17b2fbd7b81dec1d987231c4cc398fe94a3e03f
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2022-01-27 16:38:20 +0530

    1

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,5 +2,7 @@ const { useState } = require('react')
 
 module.exports = function () {
     let [count, setCount] = useState(0)
-    return <button onClick={() => setCount(count + 1)}>This Button Is Clicked {count} Times</button>
+    return (
+        <button onClick={() => setCount(count + 1)}>This Button Is Clicked {count} Times</button>
+    )
 };
