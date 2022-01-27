ef1261ba59e59723ad857deda9a6fce1dbb5004c
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2022-01-27 16:01:55 +0530

    `

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = require('express')()
 .set('views', __dirname + '/pages')
 .engine('jsx', require('express-react-views').createEngine())
 .get('/', (req, res) => {
-    res.render('index', { name: 'React' })
+    res.render('index')
 })
 .listen(8000, null, () => console.log('[Connected]'))
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,16 @@
-module.exports = function({name}) {
-    return <h1>Hello {name}</h1>
+module.exports = function() {
+    const [count, setCount] = require('React').useEffect(0)
+    return (
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>React.js Starter</title>
+        </head>
+        <body>
+            <button onClick={setCount(count + 1)}>This Button Is Clicked {count} Times</button>
+        </body>
+        </html>
+    )
 };
