cac35b45bc33ea115010e0aa2b6e65f48d34d72a
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2022-01-27 16:48:33 +0530

    1

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
-const React, { useState } = require('react')
+import React, { useState } from 'react'
 
-module.exports = function () {
+function index() {
     let [count, setCount] = useState(0)
     return (
         <button onClick={() => setCount(count + 1)}>This Button Is Clicked {count} Times</button>
     )
 };
+
+module.exports = index
