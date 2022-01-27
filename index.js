5e1f9bf25dd395d9cde30d1332e32e113797b39c
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2022-01-27 15:50:24 +0530

    1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello React')
+const app = express()
+.set('view engine', 'jsx')
+.engine('jsx', require('express-react-views').createEngine())
+.get('/', (req, res) => {
+    res.render('index', { name: 'React' })
+})
+.listen(8000, null, () => console.log('[Connected]'))
---
+++ pages/index.jsx
@@ -0,0 +1,3 @@
+module.exports = function({name}) {
+    return <h1>Hello {name}</h1>
+};
