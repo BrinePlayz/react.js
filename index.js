1fb30f5b525ccdb31fc899fb50c83286330d5cb7
BrineSuckz <84075196+BrinePlayz@users.noreply.github.com>
2022-02-05 12:21:26 +0530

    1

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-let count = 0
-let array = []
-((Math.random()).toString()).split('').forEach(item => {
-    if(item === '.' || item === '0') return
-    count++
-    array.push(item)
-})
 require('express')()
 .set('view engine', 'jsx')
 .set('views', __dirname + '/pages')
@@ -12,4 +5,4 @@ require('express')()
 .get('/', (req, res) => res.render('index', {
     user: { username: 'Brine' }
 }))
-.listen(Number(array.join('')), null, () => console.log('[Connected]'))
+.listen(8000), null, () => console.log('[Connected]'))
